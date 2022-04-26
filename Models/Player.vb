e5647bae1e68d26f87340abcc60d0c596e4b4056
ChristopherPhilips <mrcphilips@outlook.com>
2022-04-25 21:20:21 -0400

    Basic Functionality

---
+++ Models/Player.vb
@@ -0,0 +1,24 @@
+﻿Imports Spectre.Console
+Public Class Player
+    Property usersName As String
+    Property stillPlaying As Boolean
+    Sub ExitGame()
+        AnsiConsole.Markup($"Thansk for playing {usersName}")
+        stillPlaying = False
+
+    End Sub
+    Sub GiveName()
+        Console.Write("Name Please:")
+        Dim enteredName = Console.ReadLine()
+        usersName = enteredName
+    End Sub
+    Sub Help()
+        Dim Actions = New List(Of String) From {"GiveName", "ExitGame", "Help"}
+        For Each action In Actions
+            Console.WriteLine(action)
+        Next
+    End Sub
+
+
+
+End Class
--- Program.vb
+++ Program.vb
@@ -1,7 +1,27 @@
 Imports System
+Imports Spectre.Console
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim curPlayer = New Player With {.stillPlaying = True, .usersName = ""}
+
+        While curPlayer.stillPlaying
+            If curPlayer.usersName = "" Then
+                curPlayer.GiveName()
+            Else
+                Console.WriteLine($"What would you like to do {curPlayer.usersName}?")
+                Dim input = Console.ReadLine()
+                CallByName(curPlayer, input, Microsoft.VisualBasic.CallType.Method)
+            End If
+        End While
+
     End Sub
+
+    Private Class manu
+
+    End Class
+
+
+
+
 End Module
